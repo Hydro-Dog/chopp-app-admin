139e6de6ed9fbbf58222b96a75f121a23e8e0d7c
ashvachkin <gloriousex.x@gmail.com>
2024-12-09 23:09:37 +0300

    fix: исправлена кнопка редактировать

--- src/pages/settings/pages/pricing-settings/price-settings-edit-body.tsx
+++ src/pages/settings/pages/pricing-settings/price-settings-edit-body.tsx
@@ -1,28 +1,39 @@
-import { Descriptions } from 'antd';
-import type { DescriptionsProps } from 'antd';
+import { useTranslation } from 'react-i18next';
+import { InputNumber, Checkbox, Tooltip, Alert, Form, Card } from 'antd';
 
-const items: DescriptionsProps['items'] = [
-  {
-    key: '1',
-    label: 'Средняя стоимость доставки',
-    children: '123123123',
-  },
-  {
-    key: '2',
-    label: 'Бесплатная доставка',
-    children: 'Включена',
-  },
-  {
-    key: '3',
-    label: 'Цена',
-    children: '333333',
-  },
-];
-
-export const PriceSettingsEditBody: React.FC = () => {
+export const PriceSettingsEditBody = () => {
+  const { t } = useTranslation();
   return (
     <div>
-      <Descriptions column={1} size={'default'} items={items} />
+      <Form.Item label={<span>{t('PRICING_PAGE.AVERAGE_COST')}</span>}>
+        <Tooltip title={t('PRICING_PAGE.DELIVERY_COMMENT')}>
+          <InputNumber
+            type="number"
+            min={0}
+            style={{ width: '30%' }}
+            placeholder={t('PRICING_PAGE.ENTER_PRICE')}
+          />
+        </Tooltip>
+      </Form.Item>
+      <Alert
+        type="info"
+        message={t('PRICING_PAGE.PRICE_COMMENT')}
+        showIcon
+        style={{ marginBottom: 16, width: '30%' }}
+      />
+      <Form.Item>
+        <Checkbox>{t('PRICING_PAGE.FREE_SHIPPING')}</Checkbox>
+      </Form.Item>
+      <Form.Item label={<span>{t('PRICE')}</span>}>
+        <Tooltip title={t('PRICING_PAGE.PRICE_DELIVERY')}>
+          <InputNumber
+            type="number"
+            min={0}
+            style={{ width: '30%' }}
+            placeholder={t('PRICING_PAGE.ENTER_PRICE')}
+          />
+        </Tooltip>
+      </Form.Item>{' '}
     </div>
   );
 };
--- src/pages/settings/pages/pricing-settings/price-settings-view-body.tsx
+++ src/pages/settings/pages/pricing-settings/price-settings-view-body.tsx
@@ -1,37 +1,28 @@
-import { useTranslation } from 'react-i18next';
-import { InputNumber, Checkbox, Tooltip, Alert, Form, Card } from 'antd';
+import { Descriptions } from 'antd';
+import type { DescriptionsProps } from 'antd';
 
-export const PriceSettingsViewBody = () => {
-  const { t } = useTranslation();
+const items: DescriptionsProps['items'] = [
+  {
+    key: '1',
+    label: 'Средняя стоимость доставки',
+    children: '123123123',
+  },
+  {
+    key: '2',
+    label: 'Бесплатная доставка',
+    children: 'Включена',
+  },
+  {
+    key: '3',
+    label: 'Цена',
+    children: '333333',
+  },
+];
+
+export const PriceSettingsViewBody: React.FC = () => {
   return (
     <div>
-      <Form.Item label={<span>{t('PRICING_PAGE.AVERAGE_COST')}</span>}>
-        <Tooltip title={t('PRICING_PAGE.DELIVERY_COMMENT')}>
-          <InputNumber
-            min={0}
-            style={{ width: '30%' }}
-            placeholder={t('PRICING_PAGE.ENTER_PRICE')}
-          />
-        </Tooltip>
-      </Form.Item>
-      <Alert
-        type="info"
-        message={t('PRICING_PAGE.PRICE_COMMENT')}
-        showIcon
-        style={{ marginBottom: 16, width: '30%' }}
-      />
-      <Form.Item>
-        <Checkbox>{t('PRICING_PAGE.FREE_SHIPPING')}</Checkbox>
-      </Form.Item>
-      <Form.Item label={<span>{t('PRICE')}</span>}>
-        <Tooltip title={t('PRICING_PAGE.PRICE_DELIVERY')}>
-          <InputNumber
-            min={0}
-            style={{ width: '30%' }}
-            placeholder={t('PRICING_PAGE.ENTER_PRICE')}
-          />
-        </Tooltip>
-      </Form.Item>{' '}
+      <Descriptions column={1} size={'default'} items={items} />
     </div>
   );
 };

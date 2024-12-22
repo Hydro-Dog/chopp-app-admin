26dbd92edc996c0884c01a70b4aa61a014177a4e
ashvachkin <gloriousex.x@gmail.com>
2024-12-22 23:32:14 +0300

    fix: потерял кнопку сохранить

--- src/pages/settings/pages/pricing-settings/components/price-settings-title/price-settings-title.tsx
+++ src/pages/settings/pages/pricing-settings/components/price-settings-title/price-settings-title.tsx
@@ -1,16 +1,17 @@
 import { useTranslation } from 'react-i18next';
 import { Button, Flex, Space, Typography } from 'antd';
-
 const { Title } = Typography;
 
 type Props = {
   isEditing: boolean;
   toggleEditMode: () => void;
 };
-
 export const PriceSettingsTitle = ({ isEditing, toggleEditMode }: Props) => {
   const { t } = useTranslation();
-  console.log('isEditing:', isEditing);
+  const onSave = () => {
+    toggleEditMode();
+  };
+
   return (
     <Flex className="w-full" justify="space-between" align="center">
       <Title className="!m-0" level={4}>
@@ -20,6 +21,9 @@ export const PriceSettingsTitle = ({ isEditing, toggleEditMode }: Props) => {
         {isEditing ? (
           <Space>
             <Button onClick={toggleEditMode}>{t('CANCEL')}</Button>
+            <Button type="primary" onClick={onSave}>
+              {t('SAVE')}
+            </Button>
           </Space>
         ) : (
           <Button type="primary" onClick={toggleEditMode}>

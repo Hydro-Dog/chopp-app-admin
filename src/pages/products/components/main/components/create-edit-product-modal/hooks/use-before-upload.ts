66b70499050b49d6c6110da8f1e60f988f5404e4
ashvachkin <gloriousex.x@gmail.com>
2025-03-07 13:08:12 +0300

    fix: исправление модалки создания товара #DEV-44

--- src/pages/products/components/main/components/create-edit-product-modal/hooks/use-before-upload.ts
+++ src/pages/products/components/main/components/create-edit-product-modal/hooks/use-before-upload.ts
@@ -2,13 +2,29 @@ import { useTranslation } from 'react-i18next';
 import { useNotificationContext } from '@shared/context';
 import { Upload } from 'antd';
 
-// Обработчик загрузки файла, который просто сохраняет файлы в состояние
-// TODO: добавить вычисление хэша, чтобы проверить нет ли уже такого изображения в загруженных
+const isImage = (file: File): Promise<boolean> => {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const arr = new Uint8Array(e.target?.result as ArrayBuffer).subarray(0, 4);
+      let header = '';
+      for (let i = 0; i < arr.length; i++) {
+        header += arr[i].toString(16);
+      }
+      const isImage =
+        header === 'ffd8ffe0' || // JPEG
+        header === '89504e47'; // PNG
+      resolve(isImage);
+    };
+    reader.readAsArrayBuffer(file.slice(0, 4));
+  });
+};
+
 export const useBeforeUpload = () => {
   const { showErrorNotification } = useNotificationContext();
   const { t } = useTranslation();
 
-  return (file: { type: string; size: number }) => {
+  return async (file: File) => {
     const isJpgOrPng =
       file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'image/jpg';
     if (!isJpgOrPng) {
@@ -16,16 +32,27 @@ export const useBeforeUpload = () => {
         message: t('ERROR'),
         description: t('ERRORS.IMAGE_INVALID_FORMAT', { format: 'JPG, JPEG, PNG' }),
       });
+      return Upload.LIST_IGNORE;
     }
+
+    const isImageFile = await isImage(file);
+    if (!isImageFile) {
+      showErrorNotification({
+        message: t('ERROR'),
+        description: t('ERRORS.IMAGE_INVALID_CONTENT'),
+      });
+      return Upload.LIST_IGNORE;
+    }
+
     const isLt2M = file.size / 1024 / 1024 < 2;
     if (!isLt2M) {
       showErrorNotification({
         message: t('ERROR'),
         description: t('ERRORS.IMAGE_TOO_BIG', { mb: 2 }),
       });
+      return Upload.LIST_IGNORE;
     }
 
-    // Возвращаем false, чтобы предотвратить автоматическую загрузку
-    return isJpgOrPng && isLt2M ? false : Upload.LIST_IGNORE;
+    return false;
   };
 };

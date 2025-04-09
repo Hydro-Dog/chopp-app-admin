d652fe08518b7933902dd545892fe16a8986c2bf
Vlad Babaev <vladbabaev11@gmail.com>
2025-04-09 19:52:11 +0300

    ref: доработки страницы настроек #DEV-0

--- src/pages/settings/settings-page.tsx
+++ src/pages/settings/settings-page.tsx
@@ -1,18 +1,44 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ExclamationCircleFilled } from '@ant-design/icons';
 import {
   ClientAppConfig,
+  CustomModal,
   TitlePage,
   useNotificationContext,
   useSuperDispatch,
   useThemeToken,
 } from '@shared/index';
 import { fetchClientAppConfig, postClientAppConfig } from '@store/slices';
-import { Row, Col, Typography, Button } from 'antd';
+import { Row, Col, Typography, Button, Modal, Flex } from 'antd';
 import { SettingCard } from './components';
 import { useGetSettings } from './hooks';
+import { useBoolean } from 'usehooks-ts';
 
-const { Paragraph, Title } = Typography;
+const { confirm } = Modal;
+const { Paragraph, Title, Text } = Typography;
+
+type Args = {
+  onOk: () => void;
+  onCancel: () => void;
+};
+
+const useShowDisableConfirm = ({ onOk, onCancel }: Args) => {
+  const { t } = useTranslation();
+
+  return () => {
+    confirm({
+      title: `${t('SETTINGS_PAGE.TURN_APP_OFF_TITLE_FULL')}?`,
+      icon: <ExclamationCircleFilled />,
+      content: t('SETTINGS_PAGE.TURN_APP_OFF_DESCRIPTION_FULL'),
+      okText: t('YES'),
+      okType: 'danger',
+      cancelText: t('NO'),
+      onOk,
+      onCancel,
+    });
+  };
+};
 
 export const SettingsPage = () => {
   const [isAppDisabled, setIsAppDisabled] = useState(false);
@@ -25,6 +51,11 @@ export const SettingsPage = () => {
   >();
   const { superDispatch: fetchClientAppConfigDispatch } = useSuperDispatch<ClientAppConfig, void>();
   const { showErrorNotification } = useNotificationContext();
+  const { value: open, setTrue: openModal, setFalse: closeModal } = useBoolean();
+
+  const showDisableConfirm = useShowDisableConfirm({
+    onOk: () => onSwitchAppToggler(!isAppDisabled),
+  });
 
   useEffect(() => {
     fetchClientAppConfigDispatch({
@@ -57,47 +88,70 @@ export const SettingsPage = () => {
   };
 
   return (
-    <TitlePage breadcrumbs title={t('SETTINGS')}>
-      <Row gutter={[24, 24]} className="px-2">
-        {settingsList.map((item) => (
-          <Col key={item.path}>
-            <SettingCard
-              path={item.path}
-              title={item.title}
-              image={item.image}
-              description={item.description}
-            />
-          </Col>
-        ))}
-      </Row>
-      <div
-        className={`p-4 rounded-xl fixed bottom-10 right-4 border-2`}
-        style={{
-          borderColor: 'none',
-          border: `1px solid ${isAppDisabled ? themeToken.colorError : themeToken.colorWarning}`,
-          background: isAppDisabled ? themeToken.colorErrorBg : themeToken.colorWarningBg,
-        }}>
-        <Title level={5}>
-          {t(
-            isAppDisabled ? 'SETTINGS_PAGE.TURN_APP_ON_TITLE' : 'SETTINGS_PAGE.TURN_APP_OFF_TITLE',
-          )}
-          ?
-        </Title>
-        <Paragraph>
-          {t(
-            isAppDisabled
-              ? 'SETTINGS_PAGE.TURN_APP_ON_DESCRIPTION'
-              : 'SETTINGS_PAGE.TURN_APP_OFF_DESCRIPTION',
-          )}
-        </Paragraph>
-        <Button
-          onClick={() => {
-            onSwitchAppToggler(!isAppDisabled);
-          }}
-          danger>
-          {t(isAppDisabled ? 'TURN_ON' : 'TURN_OFF')}
-        </Button>
-      </div>
-    </TitlePage>
+    <>
+      <TitlePage breadcrumbs title={t('SETTINGS')}>
+        <Row gutter={[24, 24]} className="px-2">
+          {settingsList.map((item) => (
+            <Col key={item.path}>
+              <SettingCard
+                path={item.path}
+                title={item.title}
+                image={item.image}
+                description={item.description}
+              />
+            </Col>
+          ))}
+        </Row>
+        <div
+          className={`p-4 rounded-xl fixed bottom-10 right-4 border-2`}
+          style={{
+            borderColor: 'none',
+            border: `1px solid ${isAppDisabled ? themeToken.colorError : themeToken.colorWarning}`,
+            background: isAppDisabled ? themeToken.colorErrorBg : themeToken.colorWarningBg,
+          }}>
+          <Title level={5}>
+            {t(
+              isAppDisabled
+                ? 'SETTINGS_PAGE.TURN_APP_ON_TITLE'
+                : 'SETTINGS_PAGE.TURN_APP_OFF_TITLE',
+            )}
+            ?
+          </Title>
+          <Paragraph>
+            {t(
+              isAppDisabled
+                ? 'SETTINGS_PAGE.TURN_APP_ON_DESCRIPTION'
+                : 'SETTINGS_PAGE.TURN_APP_OFF_DESCRIPTION',
+            )}
+          </Paragraph>
+          <Button
+            onClick={() => {
+              if (!isAppDisabled) {
+                openModal();
+              } else {
+                onSwitchAppToggler(!isAppDisabled);
+              }
+            }}
+            danger>
+            {t(isAppDisabled ? 'TURN_ON' : 'TURN_OFF')}
+          </Button>
+        </div>
+      </TitlePage>
+
+      <CustomModal
+        title={`${t('SETTINGS_PAGE.TURN_APP_OFF_TITLE_FULL')}?`}
+        open={open}
+        onOk={() => {
+          onSwitchAppToggler(!isAppDisabled);
+          closeModal();
+        }}
+        onCancel={closeModal}
+        okTitle={t('YES')}
+        okColor="danger">
+        <Flex align="center" gap={16}>
+          <Text>{t('SETTINGS_PAGE.TURN_APP_OFF_DESCRIPTION_FULL')}</Text>
+        </Flex>
+      </CustomModal>
+    </>
   );
 };
